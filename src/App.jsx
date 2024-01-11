114c3d2a9d7f19aa37272474708882751bf003b4
Taiwo Jolomi <taiwojolomi15@gmail.com>
2024-01-11 23:58:12 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './Header.jsx';
+
+const App = () => {
+       return (
+              <div className='container'>
+                     <Header />
+                     <div className="container-view">Some activities should be here</div>
+              </div>
+       );
+};
+
+export default App
